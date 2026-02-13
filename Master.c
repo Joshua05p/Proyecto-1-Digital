d2b6d01a4b7a766234bc4f753d4b9de0ddefacc2
Joshua05p <per23395@uvg.edu.gt>
2026-02-13 16:46:13 -0600

    Update Master.c

--- Master.c
+++ Master.c
@@ -5,8 +5,8 @@
  * Author : masco
  */ 
 
-#include <avr/io.h>
 #define F_CPU 16000000
+#include <avr/io.h>
 #define slave1 0x30
 #define slave2 0x40
 
@@ -19,6 +19,8 @@
 #include <avr/io.h>
 #include "Libreria.h"
 #include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 //////////////////////////////////////////////////////////////
 ////////////////////PROTOTIPOS////////////////////////////////
@@ -28,152 +30,126 @@ uint8_t bufferI2C;
 uint8_t bufferI2C2;
 void refreshPort(uint8_t valor);
 void setup();	
+char texto1[4];
+char texto2[5];
+char buffer[8];
+uint8_t posicion = 0;
 
+uint16_t motor1 = 0;
+uint16_t motor2 = 0;
+uint16_t motor3 = 0;
 
+uint16_t extraer_numero(const char *msg) {
+	return atoi(&msg[3]);
+}
 
 int main(void)
 {
-    setup();
+	initLCD8bits();
+
+	LCD_SET(0,1);
+	LCD_STRING("S1");
+
+	LCD_SET(6,1);
+	LCD_STRING("S2");
+	
+	LCD_SET(12,1);
+	LCD_STRING("S3");
+	
+	iniciar_USART(103);
 	I2C_master_init(100000, 1); //Inicializar el master, Frecuencia 100kHz, Prescaler 1
 
     while (1) 
     {
 		//Esclavo 1
-		PORTB |= (1<<PORTB5);
-		
-		if(!I2C_master_start()) return;
+		if(!I2C_master_start()) continue;
 		
 		if(!I2C_master_write(slave1W)){
 			I2C_master_Stop();
-			 return;
+			 continue;
 		}
 		
 		I2C_master_write('R');
 				
 		if(!I2C_master_repeatedStar()){
 			I2C_master_Stop();
-			return;
+			continue;
 		}
 		
 		if(!I2C_master_write(slave1R)){
 			I2C_master_Stop();
-			return;
+			continue;
 		}
 		
 		I2C_master_read(&bufferI2C, 0);
 		I2C_master_Stop();
 		
-		PORTB &= ~(1<<PORTB5);
+		_delay_ms(100);
 		
-		refreshPort(bufferI2C);
-		
+		itoa(bufferI2C, texto1, 10);
+		LCD_STRING(texto1);
+		cadena("Valor slave 1: ");
+		cadena(texto1);
+		cadena("%");
 		//Esclavo 2
-
-		PORTB |= (1<<PORTB5);
 		
-		if(!I2C_master_start()) return;
+		if(!I2C_master_start()) continue;
 		
 		if(!I2C_master_write(slave2W)){
 			I2C_master_Stop();
-			return;
+			continue;
 		}
 		
 		I2C_master_write('D');
 		
 		if(!I2C_master_repeatedStar()){
 			I2C_master_Stop();
-			return;
+			continue;
 		}
 		
 		if(!I2C_master_write(slave2R)){
 			I2C_master_Stop();
-			return;
+			continue;
 		}
 		
 		I2C_master_read(&bufferI2C2, 0);
 		I2C_master_Stop();
 		
-		PORTB &= ~(1<<PORTB5);
-		refreshPort2(bufferI2C2);
-		
-		_delay_ms(500);
-
-		LCD_SET(0,2);
-		LCD_STRING(bufferI2C);
-		_delay_ms(50);
-		cadena("Valor slave 1:");
-		cadena(bufferI2C); 
-		
 		cadena("         ");
-		
-		LCD_SET(0,6);
-		LCD_STRING(bufferI2C2);
 		_delay_ms(50);
-		cadena("Valor slave 2:");
-		cadena(bufferI2C2);
+		itoa(bufferI2C2, texto2, 10);
+		LCD_STRING(texto2);
+		cadena("Valor slave 2: ");
+		cadena(texto2);
 		cadena("\n");
+		_delay_ms(500);
 		
-}
+				
+	}
 }
 ///////////////////////////////////////////////////
 //////////////////FUNCIONES///////////////////////
 
-void setup(){
-	DDRD |= (1<<DDD2)|(1<<DDD3)|(1<<DDD4)|(1<<DDD5)|(1<<DDD6)|(1<<DDD7);
-	DDRB |= (1<<DDB0)|(1<<DDB1);
-	
-	DDRB |= (1<<DDB5);
-	
-	PORTB &= ~((1<<PORTB0)|(1<<PORTB1));
-	PORTD &= ~((1<<PORTD2)|(1<<PORTD3)|(1<<PORTD4)|(1<<PORTD5)|(1<<PORTD6)|(1<<PORTD7));
-	
+ISR(USART_RX_vect) {
+		char c = UDR0;
+		if (c == '#') {
+			buffer[posicion] = '\0'; // Finaliza cadena
+			posicion = 0;
+
+			if (buffer[0] == 'M' && buffer[1] == '1') {
+				motor1 = extraer_numero((const char *)buffer);
+				
+				
+				} else if (buffer[0] == 'M' && buffer[1] == '2') {
+				motor2 = extraer_numero((const char *)buffer);
+				
+				
+				} else if (buffer[0] == 'M' && buffer[1] == '3') {
+				motor3 = extraer_numero((const char *)buffer);
+				
+				
+				} 
+			} else {
+			buffer[posicion++] = c;
+		}
 }
-
-void refreshPort(uint8_t valor){
-	if(valor & 0b10000000){
-		PORTB |= (1<<PORTB1);
-		}else{
-		PORTB &= ~(1<<PORTB1);
-	}
-	if(valor & 0b01000000){
-		PORTB |= (1<<PORTB0);
-		}else{
-		PORTB &= ~(1<<PORTB0);
-	}
-	if(valor & 0b00100000){
-		PORTD |= (1<<PORTD7);
-		}else{
-		PORTD &= ~(1<<PORTD7);
-	}
-	if(valor & 0b00010000){
-		PORTD |= (1<<PORTD6);
-		}else{
-		PORTD &= ~(1<<PORTD6);
-	}
-
-}
-void refreshPort2(uint8_t valor){
-	if(valor & 0b00001000){
-		PORTD |= (1<<PORTD5);
-		}else{
-		PORTD &= ~(1<<PORTD5);
-	}
-	if(valor & 0b00000100){
-		PORTD |= (1<<PORTD4);
-		}else{
-		PORTD &= ~(1<<PORTD4);
-	}
-	if(valor & 0b00000010){
-		PORTD |= (1<<PORTD3);
-		}else{
-		PORTD &= ~(1<<PORTD3);
-	}
-	if(valor & 0b00000001){
-		PORTD |= (1<<PORTD2);
-		}else{
-		PORTD &= ~(1<<PORTD2);
-	}
-
-}
-	
-
